14c5a744ab5c56a39583d8eb2cd961636ef39b8b
xumengyin <xumengyin>
2021-06-28 18:38:14 +0800

    h

---
+++ app/src/main/java/com/jerry/wifimaster/ui/NetSpeedActivity.kt
@@ -0,0 +1,15 @@
+package com.jerry.wifimaster.ui
+
+import com.jerry.baselib.base.BaseAdActivity
+import com.jerry.wifimaster.R
+
+class NetSpeedActivity : BaseAdActivity() {
+
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_net_speed
+    }
+
+
+
+}
